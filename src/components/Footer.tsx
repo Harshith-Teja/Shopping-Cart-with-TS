75a92e182db69c5d24d77a824de29ddd58a6097f
Harshith Teja <harshithtejadontoju@gmail.com>
2024-12-01 17:30:48 +0530

    Built the productList and product comps. Created useCart, useProduct custom hooks and wrapped the app with cart & product providers

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,25 @@
+import useCart from "../hooks/useCart";
+
 type FooterProps = {
   viewCart: boolean;
 };
+
 const Footer = ({ viewCart }: FooterProps) => {
-  return <div>Footer</div>;
+  const { totalItems, totalPrice } = useCart();
+
+  const year: number = new Date().getFullYear();
+
+  const pageContent = viewCart ? (
+    <p>Shopping Cart &copy; {year}</p>
+  ) : (
+    <>
+      <p>Total Items: {totalItems}</p>
+      <p>Total Price: {totalPrice}</p>
+      <p>Shopping Cart &copy; {year}</p>
+    </>
+  );
+
+  return <footer className="footer">{pageContent}</footer>;
 };
 
 export default Footer;
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,4 @@
+import useCart from "../hooks/useCart";
 import Nav from "./Nav";
 
 type HeaderProps = {
@@ -5,14 +6,16 @@ type HeaderProps = {
   setViewCart: React.Dispatch<React.SetStateAction<boolean>>;
 };
 const Header = ({ viewCart, setViewCart }: HeaderProps) => {
+  const { totalItems, totalPrice } = useCart();
+
   return (
     <div>
       <header className="header">
         <div className="header__title-bar">
           <h1>Acme Co.</h1>
           <div className="header__price-box">
-            <p>Total Items:</p>
-            <p>Total Price:</p>
+            <p>Total Items: {totalItems}</p>
+            <p>Total Price: {totalPrice}</p>
           </div>
         </div>
         <Nav viewCart={viewCart} setViewCart={setViewCart} />
