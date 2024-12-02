7133659fc699cca9b0533ed0f6f18d42852a4d35
Harshith Teja <harshithtejadontoju@gmail.com>
2024-12-02 14:34:34 +0530

    built Cart and CartLineItem comps for Cart page

--- src/context/CartProvider.tsx
+++ src/context/CartProvider.tsx
@@ -82,9 +82,6 @@ const reducer = (
       return { ...state, cart: [...filteredCart, updatedItem] };
     }
     case REDUCER_ACTION_TYPE.SUBMIT: {
-      if (!action.payload)
-        throw new Error("action.payload missing in SUBMIT action");
-
       return { ...state, cart: [] };
     }
     default:
